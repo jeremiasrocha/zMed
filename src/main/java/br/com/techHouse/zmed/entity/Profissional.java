2344c69cdcbfc1746736500b0577098dca3f3cc6
Jerê <Jer@192.168.1.5>
2018-09-20 01:42:47 -0300

    Implementações Diversas.

--- src/main/java/br/com/techHouse/zmed/entity/Profissional.java
+++ src/main/java/br/com/techHouse/zmed/entity/Profissional.java
@@ -277,4 +277,29 @@ public class Profissional implements Serializable {
 		this.valorAtendimento = valorAtendimento;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Profissional other = (Profissional) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
